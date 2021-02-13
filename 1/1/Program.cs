76636fc5a5e465f1817254400beee253f8c0a0f5
Vargos123 <bacahoriyk@gmail.com>
2021-02-13 15:19:21 +0200

    first commit

---
+++ 1/1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Calculate(int a, int b, int c)
+        {
+            Console.WriteLine("Среднее арифметическое: " + (a + b + c) / 3);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Введите 1 значение");
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число");
+            }
+            Console.WriteLine("Введите 2 значение");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число");
+            }
+            Console.WriteLine("Введите 3 значение");
+            while (!int.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число");
+            }
+
+            Calculate(a, b, c);
+        }
+    }
+}
---
+++ 2/2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _2
+{
+
+    public delegate int Operation(int x, int y);
+
+    class Program
+    {
+
+        private static int Add(int x, int y) 
+        {
+            return x + y;
+        }
+        private static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        private static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        private static int Dell(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                int x, y;
+                Console.WriteLine("Введите первое число:");
+                while (!int.TryParse(Console.ReadLine(), out x))
+                {
+                    Console.WriteLine("Ошибка! Введите целое число:");
+                }
+                Console.WriteLine("Введите второе число:");
+                while (!int.TryParse(Console.ReadLine(), out y))
+                {
+                    Console.WriteLine("Ошибка! Введите целое число:");
+                }
+                Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+                string a = Console.ReadLine();
+                switch (a)
+                {
+                    case "+":
+                        Operation add = Add;
+                        Console.WriteLine("Ответ: " + add(x, y));
+                        break;
+                    case "-":
+                        Operation sub = Sub;
+                        Console.WriteLine("Ответ: " + sub(x, y));
+                        break;
+                    case "*":
+                        Operation mult = Mult;
+                        Console.WriteLine("Ответ: " + mult(x, y));
+                        break;
+                    case "/":
+                        if (y == 0)
+                            Console.WriteLine("На 0 делить нельзя!");
+                        else
+                        {
+                            Operation dell = Dell;
+                            Console.WriteLine("Ответ: " + dell(x, y));
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Выбрана ошибочная операция!");
+                        break;
+                }
+
+                Console.ReadKey();
+            }            
+        }
+    }
+}
